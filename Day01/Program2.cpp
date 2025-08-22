d10039c8f93e041eacc58f5d9a71923c9230ed29
Charu Tiwari <charutiwari.014@gmail.com>
2025-08-22 23:48:29 +0530

    Copied assignment

---
+++ Day01/Program2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>//Iostream headerfile included
+using namespace std;//Between so many namespaces we need to choose std as input and output functions which are gonna be covered later are written in std
+
+int main(){  //The starting point of the code
+
+    //Scope of main function
+
+    return 0; //Program successfully executed
+}
