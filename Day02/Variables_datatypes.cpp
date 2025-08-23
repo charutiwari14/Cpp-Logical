9af03dc1a7fda97291d46f35d5545fcab0afa6d8
Charu Tiwari <charutiwari.014@gmail.com>
2025-08-24 01:29:23 +0530

    Lesson 2 updated

---
+++ Day02/Variables_datatypes.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int datatype
+    int a = 14;
+    cout << "a is a variable and int is a datatype" << endl;
+    // char datatype
+    char ch = 'A';
+    cout << "ch is a variable and char is a datatype" << endl;
+
+    // float datatype
+    float pi = 3.14;
+    cout << "pi is a variable and float is a datatype" << endl;
+
+    // long datatype
+    long l = 1404;
+    cout << "l is a variable and long is a datatype" << endl;
+
+    // double datatype
+    double d = 140402;
+    cout << "d is a variable and double is a datatype" << endl;
+
+    cout << endl;
+    cout << "Endl is similar to /n.Both will change the line." << endl<<endl;
+
+    cout << "Rules of declaring a variable:";
+
+    cout << "1.Every variable must have a data type (e.g., int, float, char)." << endl;
+
+    cout << "2.Names can include letters, digits, and underscores." << endl;
+
+    cout << "3.A name cannot start with a digit." << endl;
+
+    cout << "4.Spaces are not allowed in variable names." << endl;
+
+    cout << "5.C++ is case-sensitive (num, Num, NUM are different)." << endl;
+
+    cout << "6.Keywords (like int, for, while) cannot be used as names." << endl;
+
+    cout << "7.It can be initialized at declaration." << endl;
+
+    cout << "8.Always generate meaningful, descriptive names for readability." << endl;
+
+    cout << "9.Local variables exist inside functions; global variables exist outside." << endl;
+
+    cout << "10.Following rules avoids errors and improves code quality." << endl;
+}
