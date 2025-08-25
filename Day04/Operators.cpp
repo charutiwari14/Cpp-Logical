e95ca37c7656a67dfe734004cef7e64a87995f68
Charu Tiwari <charutiwari.014@gmail.com>
2025-08-25 13:34:32 +0530

    Arithmetic and Relational Operators Completed

---
+++ Day04/Operators.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<endl<<"Arithmetic Operators:Mathematical Operations"<<endl;
+    int a=5,b=3;
+    cout<<"Addition(+):"<<a+b<<endl;
+    cout<<"Subtraction(-):"<<a-b<<endl;
+    cout<<"Multiplication(*):"<<a*b<<endl;
+    cout<<"Division(/):"<<a/b<<endl;
+    cout<<"Modulus Operator(%):"<<a%b<<endl;
+
+    cout<<endl;
+    cout<<"Relational Operators:";
+    int c=8,d=17;
+    cout<<"It returns 0 or 1, is used in conditionals and loops mostly"<<endl;
+    cout<<"(c==d)="<<(c==d)<<" Equals to"<<endl;
+    cout<<"(c>=d)="<<(c>=d)<<" Greater than,Equals to"<<endl;
+    cout<<"(c<=d)="<<(c<=d)<<" Lesser than, Equals to"<<endl;
+    cout<<"(c!=d)="<<(c!=d)<<" Not Equals to"<<endl;
+    cout<<"(c>d)="<<(c>d)<<" Greater than"<<endl;
+    cout<<"(c<d)="<<(c<d)<<" Lesser than"<<endl;
+
+    cout<<endl;
+}
