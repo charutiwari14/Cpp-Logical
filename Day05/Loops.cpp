f57632929e39482d9792170da43b16ad9b5bcbd8
Charu Tiwari <charutiwari.014@gmail.com>
2025-08-26 15:55:07 +0530

    Loops syntax updated

---
+++ Day05/Loops.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+// There are 4 kinds of loops in c++:
+// 1)for loop
+// 2)while loop
+// 3)do-while loop
+// 4)for each loop
+
+//1) Syntax of for loop 
+// for (Initialization; Condition; Updation)
+// {
+//     /* code */
+// }
+
+//2)Syntax for while loops
+// Initialisation
+// while (Condition)
+// {
+//     /* code */
+//     Updation
+// }
+
+//3)Syntax for do-while loops
+// Initialisation
+// do{
+//     /* code */ 
+//     Updation
+//}while (Condition)
+
+//4)Syntax for for-each loop
+// for (declaration : container) {
+    /* code */
+//}
+
